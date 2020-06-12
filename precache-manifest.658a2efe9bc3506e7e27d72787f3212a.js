db6976da476a71c8c1ade8b94a00477d7e069079
PeralStudio <info@peralstudio.com>
2020-06-12 20:22:44 +0200

    Updates

---
+++ precache-manifest.658a2efe9bc3506e7e27d72787f3212a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43ba5494a9cf878ff5396907da2f6369",
+    "url": "/BuscadorLetrasDeCanciones/index.html"
+  },
+  {
+    "revision": "c517a67033f34235a92e",
+    "url": "/BuscadorLetrasDeCanciones/static/css/main.7384ce32.chunk.css"
+  },
+  {
+    "revision": "3ca69511dd960fa13cb4",
+    "url": "/BuscadorLetrasDeCanciones/static/js/2.401f64b2.chunk.js"
+  },
+  {
+    "revision": "85b12af60ddf57ea6860c3b16407fd52",
+    "url": "/BuscadorLetrasDeCanciones/static/js/2.401f64b2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c517a67033f34235a92e",
+    "url": "/BuscadorLetrasDeCanciones/static/js/main.4cab33b7.chunk.js"
+  },
+  {
+    "revision": "f5e90643addda5d4b56c",
+    "url": "/BuscadorLetrasDeCanciones/static/js/runtime-main.bcff7094.js"
+  }
+]);
